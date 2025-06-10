e3edf31cf259e068049d0e2815f55418e40adac5
rhsaman <tradeerha@gmail.com>
2025-06-10 12:48:44 +0330

    avante added

---
+++ nvim/lua/pluginsConfig/avante.lua
@@ -0,0 +1,56 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  opts = {
+    provider = "openai",
+    providers = {
+      openai = {
+        endpoint = "http://127.0.0.1:1234/v1",
+        api_key = " ",
+        disable_tools = true,
+        extra_request_body = {
+          stream = true,
+          -- temperature = 0.7,
+          max_tokens = 8192,
+        },
+      },
+    },
+  },
+
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "echasnovski/mini.pick",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "stevearc/dressing.nvim",
+    "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      ft = { "markdown", "Avante" },
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+    },
+  },
+}
