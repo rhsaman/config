14b842e0088ca57f28e407706e795a2a6736147b
rhsaman <tradeerha@gmail.com>
2025-08-25 12:40:39 +0330

    update avante

--- nvim/lua/pluginsConfig/avante.lua
+++ nvim/lua/pluginsConfig/avante.lua
@@ -1,66 +1,51 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false,
-  opts = {
-    provider = "openai",
-    providers = {
-      openai = {
-        endpoint = "http://127.0.0.1:1234/v1",
-        api_key = "",
-        disable_tools = true,
-        extra_request_body = {
-          stream = true,
-          -- temperature = 0.7,
-          max_tokens = 8192,
-        },
-      },
-    },
-    windows = {
-      position = "right", -- the position of the sidebar
-      wrap = true,     -- similar to vim.o.wrap
-      width = 30,      -- default % based on available width
-      sidebar_header = {
-        enabled = true, -- true, false to enable/disable the header
-        align = "center", -- left, center, right for title
-        rounded = true,
-      },
-    },
-  },
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false,
+	opts = {
+		provider = "ollama", -- دقیقا اسم جدول پایین
+		vendors = {
+			ollama = {
+				__class = "avante.providers.ollama", -- کلاس رسمی provider
+				endpoint = "http://127.0.0.1:11434",
+				model = "qwen3:4b",
+			},
+		},
+	},
 
-  build = "make",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "echasnovski/mini.pick",
-    "nvim-telescope/telescope.nvim",
-    "hrsh7th/nvim-cmp",
-    "ibhagwan/fzf-lua",
-    "stevearc/dressing.nvim",
-    "folke/snacks.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "zbirenbaum/copilot.lua",
-    {
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      ft = { "markdown", "Avante" },
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-    },
-  },
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"echasnovski/mini.pick",
+		"nvim-telescope/telescope.nvim",
+		"hrsh7th/nvim-cmp",
+		"ibhagwan/fzf-lua",
+		"stevearc/dressing.nvim",
+		"folke/snacks.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"zbirenbaum/copilot.lua",
+		{
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					use_absolute_path = true,
+				},
+			},
+		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			ft = { "markdown", "Avante" },
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+		},
+	},
 }
