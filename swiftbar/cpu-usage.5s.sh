e76b3401e68b501df474151f571ae85f23115c28
rhsaman <tradeerha@gmail.com>
2025-08-25 12:41:15 +0330

    up

--- swiftbar/cpu-usage.5s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# <swiftbar.title>cpu usage</swiftbar.title>
-# <swiftbar.version>1.0</swiftbar.version>
-# <swiftbar.author>YourName</swiftbar.author>
-# <swiftbar.desc>Show CPU usage</swiftbar.desc>
-
-cpu=$(top -l 1 | awk '/CPU usage/ {print $3 + $5 "%"}')
-echo "CPU: $cpu | refresh=true"
